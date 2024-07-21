a290a8fb451150be4527247c0f2661f98ac97a9e
Abd AlRhman AlQiam <Abdurrahman.alQiam@gmail.com>
2024-07-21 19:24:29 -0400

    Update run_harp.py

--- run_harp.py
+++ run_harp.py
@@ -47,37 +47,45 @@ def loss_mlu(y_pred_batch, y_true_batch):
 if props.mode.lower() == "train":
     
     def validate(model, props, val_ds, val_dl):
+        
+        # val_ds.pte = (val_ds.pte).to(device=props.device, dtype=props.dtype)
+        # val_ds.padded_edge_ids_per_path = val_ds.padded_edge_ids_per_path.to(device=props.device)
+        
         val_norm_mlu = []
         with torch.no_grad():
             with tqdm(val_dl) as vals:
                 for i, inputs in enumerate(vals):
                     node_features, capacities, tms, tms_pred, opt = inputs
                                         
-                    node_features = node_features.to(device=device, dtype=props.dtype)
-                    capacities = capacities.to(device=device, dtype=props.dtype)
-                    tms = tms.to(device=device, dtype=props.dtype)
-                    tms_pred = tms_pred.to(device=device, dtype=props.dtype)
-                    opt = opt.to(device=device, dtype=props.dtype)
-
+                    node_features = node_features.to(device=props.device, dtype=props.dtype)
+                    capacities = capacities.to(device=props.device, dtype=props.dtype)
+                    tms = tms.to(device=props.device, dtype=props.dtype)
+                    tms_pred = tms_pred.to(device=props.device, dtype=props.dtype)
+                    opt = opt.to(device=props.device, dtype=props.dtype)
+                    
+                    # If prediction is on, feed the predicted matrix
                     if props.pred:
                         tms_pred = tms_pred.to(device=device, dtype=props.dtype)
                         
                         predicted = model(props, node_features, val_ds.edge_index, capacities,
                                 val_ds.padded_edge_ids_per_path,
-                                tms, tms_pred, train_dataset.pte)
+                                tms, tms_pred, val_ds.pte)
 
                     # If prediction is off, feed the actual matrix as predicted matrix
                     else:
                         predicted = model(props, node_features, val_ds.edge_index, capacities,
                                 val_ds.padded_edge_ids_per_path,
-                                tms, tms, train_dataset.pte)
-                    
+                                tms, tms, val_ds.pte)
+                        
                     # predicted = model(props, node_features, val_ds.edge_index, capacities,
                     #         val_ds.padded_edge_ids_per_path,
                     #         tms, tms_pred, val_ds.pte)
                     val_loss, value_loss_value = loss_mlu(predicted, opt)
                     val_norm_mlu.append(value_loss_value)
-                    
+                
+        # val_ds.pte = val_ds.pte.to(device="cpu")
+        # val_ds.padded_edge_ids_per_path = val_ds.padded_edge_ids_per_path.to(device="cpu")
+        
         return val_norm_mlu
         
     # create the dataset
@@ -135,7 +143,7 @@ if props.mode.lower() == "train":
                     capacities = capacities.to(device=device, dtype=props.dtype)
                     tms = tms.to(device=device, dtype=props.dtype)
                     opt = opt.to(device=device, dtype=props.dtype)
-
+                    
                     # If prediction is on, feed the predicted matrix
                     if props.pred:
                         tms_pred = tms_pred.to(device=device, dtype=props.dtype)
@@ -149,7 +157,7 @@ if props.mode.lower() == "train":
                         predicted = model(props, node_features, train_dataset.edge_index, capacities,
                                 train_dataset.padded_edge_ids_per_path,
                                 tms, tms, train_dataset.pte)
-    
+                    
                     loss, loss_val = loss_mlu(predicted, opt)
                     loss.backward()
                     optimizer.step()
@@ -167,7 +175,7 @@ if props.mode.lower() == "train":
             val_dataset = val_ds_list[i]
             val_dl = val_dl_list[i]
             val_dataset.pte = (val_dataset.pte).to(device=props.device, dtype=props.dtype)
-            val_dataset.padded_edge_ids_per_path = val_dataset.padded_edge_ids_per_path.to(device)
+            val_dataset.padded_edge_ids_per_path = val_dataset.padded_edge_ids_per_path.to(device=props.device)
             
             val_norm_mlu = validate(model, props, val_dataset, val_dl)
             val_avg_loss = sum(val_norm_mlu)/len(val_norm_mlu)
